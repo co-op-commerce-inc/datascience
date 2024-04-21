5cef69594a1d20905357237f23e8a4be3007b076
Peter Li <peteryushunli@gmail.com>
2024-04-21 19:43:44 -0400

    added terminator files

---
+++ terminator/training_frequency/rpl_by_impression_seq_v2.sql
@@ -0,0 +1,136 @@
+--- Find the conversions and ad spend for each widget load by order 
+with order_sessions as (
+select 
+    remote_order_id,
+    session_id,
+    min(dvce_created_tstamp) as timestamp
+from event.event
+where 
+    to_date(dvce_created_tstamp) between '2024-01-18' and current_date - 14
+group by all),
+nurture_sessions as (
+select distinct session_id
+from event.event 
+where widget_type = 'LEAD_GEN'
+)
+, email_sessions as (
+select distinct session_id
+from event.event 
+where event_name = 'server_email_product_display'
+)
+,order_session_seq as (
+select 
+    remote_order_id,
+    session_id,
+    timestamp,
+    row_number() over (partition by remote_order_id order by timestamp asc) as session_sequence_number,
+    FIRST_VALUE(timestamp) OVER (PARTITION BY remote_order_id ORDER BY timestamp ASC) AS first_session_timestamp, --- timestamp for 1st session
+    TIMESTAMPDIFF(MINUTE, FIRST_VALUE(timestamp) OVER (PARTITION BY remote_order_id ORDER BY timestamp ASC), timestamp) AS time_diff_minutes,
+    TIMESTAMPDIFF(HOUR, FIRST_VALUE(timestamp) OVER (PARTITION BY remote_order_id ORDER BY timestamp ASC), timestamp) AS time_diff_hours,
+    TIMESTAMPDIFF(DAY, FIRST_VALUE(timestamp) OVER (PARTITION BY remote_order_id ORDER BY timestamp ASC), timestamp) AS time_diff_days-- time diff
+from order_sessions 
+where 
+    session_id not in (select * from nurture_sessions)
+    and session_id not in (select * from email_sessions)
+    -- and to_date(FIRST_VALUE(timestamp) OVER (PARTITION BY remote_order_id ORDER BY timestamp ASC)) >= '2024-04-18'
+),
+session_brands as (
+select 
+    session_id, 
+    count(distinct brand_name) as advertiser_count
+from event.event e
+where 
+    brand_name is not null
+    and brand_name != publisher_name --- do not count upsell brand impr
+    and to_date(dvce_created_tstamp) >= '2024-01-18'
+group by all
+),
+session_conv as (
+--- Find events associated with a specific conversion
+select 
+    -- *
+    e.session_id,
+    -- count(distinct e.brand_name) as advertiser_count,
+    count(distinct c.order_id) as conv_count,
+    sum(a.billable_amount) as ad_spend
+from event.event e 
+left join dbt_tkaraffa.derived__conversion c using (event_id)
+left join dbt_tkaraffa.curated__ad_spend_revenue a 
+    on c.order_id = a.order_id
+where 
+    e.session_id in (
+        select distinct session_id
+        from event.event 
+        where event_id in (
+            select distinct event_id
+            from dbt_tkaraffa.derived__conversion
+            where 
+                to_date(event_created_at) between '2024-01-18' and current_date - 14
+                and conversion_type = 'cross-sell'
+            )
+    )
+group by all
+),
+granular as (
+select 
+    remote_order_id,
+    first_session_timestamp,
+    session_id,
+    timestamp,
+    session_sequence_number,
+    time_diff_minutes,
+    time_diff_hours,
+    time_diff_days,
+    case 
+      when (time_diff_hours <= 24) then 'first_day'
+      when (time_diff_hours > 24 and time_diff_hours <= 84) then 'subsequent_under_half_week'
+      when (time_diff_hours > 84) then 'subsequent_over_half_week'
+      else 'other' end as impression_sequence,
+    case when advertiser_count is null then 0 else advertiser_count end as advertiser_count,
+    case when conv_count is null then 0 else conv_count end as conv_count,
+    case when ad_spend is null then 0 else ad_spend end as ad_spend,
+from order_session_seq 
+left join session_brands sb using (session_id)
+left join session_conv using (session_id)
+)
+-- ,date_agg as (
+-- select 
+--     to_date(timestamp) as date,
+--     case 
+--       when (time_diff_hours <= 24) then 'first_day'
+--       when session_sequence_number > 1 and time_diff_hours > 24 and time_diff_hours <= 84 then 'subsequent_under_half_week'
+--       when session_sequence_number > 1 and time_diff_hours > 84 then 'subsequent_over_half_week'
+--       else 'other' end as impression_sequence,
+--     count(distinct session_id) as loads,
+--     sum(advertiser_count) as advertiser_count,
+--     sum(conv_count) as conv,
+--     sum(ad_spend) as ad_spend
+-- from granular
+-- group by all
+-- order by date asc)
+-- select 
+--     *,
+--     -- Find 7 day avg of loads partitioned by impression_sequence
+--     avg(loads) over (partition by impression_sequence order by date asc rows between 6 preceding and current row) as loads_7_day_avg,
+--     -- Find 7 day avg of advertiser_count partitioned by impression_sequence
+--     avg(advertiser_count) over (partition by impression_sequence order by date asc rows between 6 preceding and current row) as advertiser_count_7_day_avg,
+--     -- Find 7 day avg of conv partitioned by impression_sequence
+--     avg(conv) over (partition by impression_sequence order by date asc rows between 6 preceding and current row) as conv_7_day_avg,
+--     -- Find 7 day avg of ad_spend partitioned by impression_sequence
+--     avg(ad_spend) over (partition by impression_sequence order by date asc rows between 6 preceding and current row) as ad_spend_7_day_avg 
+-- from date_agg
+-- ;
+select 
+    *
+from granular
+-- where 
+--     remote_order_id in (
+--         select distinct remote_order_id
+--         from granular
+--         where 
+--             session_sequence_number > 1
+--             and ad_spend > 0
+--     )
+order by remote_order_id, session_sequence_number asc
+limit 500;
+
